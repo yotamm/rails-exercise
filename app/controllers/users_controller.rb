829408a150ca3086654c3c33aa563f95afee3724
Yotam <yotam.ronen@lemonade.com>
2020-10-10 16:19:17 +0300

    404 handler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
-        render json: @user
+        @user = User.find_by(id: params[:id]) or not_found
     end
 
     def new
